03199057478ad122fcfe178231148a6238179df3
Kashish Jain <kashish.jain@saint-gobain.com>
2023-12-18 17:15:49 +0530

    Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+//import React from 'react'
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <>
+            <header className="bg-slate-200 shadow-md mx-auto">
+                <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                    <Link to='/'>
+                        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                            <span className="text-slate-600">Kash</span>
+                            <span className="text-slate-800">Estate</span>
+                        </h1>
+                    </Link>
+                    <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                        <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64"></input>
+                        <FaSearch className="text-slate-600" />
+                    </form>
+                    <ul className="flex gap-3">
+                        <Link to='/'><li className="hover:underline">Home</li></Link>
+                        <Link to='/about'><li className="hover:underline">About</li></Link>
+                        <Link to='/sign-in'><li className="hover:underline">Sign In</li></Link>
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
